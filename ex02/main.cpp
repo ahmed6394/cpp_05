2f627ed6470e68730f9274a19ff67cf523b4b4a9
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2025-08-20 16:38:52 +0200

    ex02: presidential added in main

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -6,19 +6,25 @@
 /*   By: gahmed <gahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 12:43:03 by gahmed            #+#    #+#             */
-/*   Updated: 2025/08/15 14:52:22 by gahmed           ###   ########.fr       */
+/*   Updated: 2025/08/20 16:35:52 by gahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/Bureaucrat.hpp"
 #include "inc/ShrubberyCreationForm.hpp"
+#include "inc/PresidentialPardonForm.hpp"
 
 int main()
 {	std::cout << "\n==========Bureaucrat Creation========\n";
-	Bureaucrat alice("Alic", 130);
+	Bureaucrat alice("Alic", 140);
 
 	ShrubberyCreationForm bob("employee");
 
 	alice.signForm(bob);
 	alice.executeForm(bob);
+
+	PresidentialPardonForm putin("President");
+	alice.signForm(putin);
+	alice.executeForm(putin);
+
 }
--- ex02/src/PresidentialPardonForm.cpp
+++ ex02/src/PresidentialPardonForm.cpp
@@ -6,16 +6,16 @@
 /*   By: gahmed <gahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 12:43:13 by gahmed            #+#    #+#             */
-/*   Updated: 2025/08/20 15:08:19 by gahmed           ###   ########.fr       */
+/*   Updated: 2025/08/20 16:38:08 by gahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/PresidentialPardonForm.hpp"
 
-PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialForm", 139, 120), target("Default")
+PresidentialPardonForm::PresidentialPardonForm() : AForm("PresidentialForm", 25, 5), target("Default")
 {}
 
-PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("PresidentialForm", 120, 123), target(target) 
+PresidentialPardonForm::PresidentialPardonForm(std::string target) : AForm("PresidentialForm", 25, 5), target(target) 
 {}
 
 PresidentialPardonForm::PresidentialPardonForm(const PresidentialPardonForm &obj) : AForm(obj), target(obj.target) 
