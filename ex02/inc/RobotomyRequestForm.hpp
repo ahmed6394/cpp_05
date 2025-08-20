845bc39ee936ae3f62e373df7089944d53fa95c3
Mahabub Ahmed <mahabub.ahmed.eee@gmail.com>
2025-08-20 18:21:53 +0200

    ex02: robo init

--- ex02/inc/RobotomyRequestForm.hpp
+++ ex02/inc/RobotomyRequestForm.hpp
@@ -6,7 +6,24 @@
 /*   By: gahmed <gahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 12:43:33 by gahmed            #+#    #+#             */
-/*   Updated: 2025/08/05 12:43:34 by gahmed           ###   ########.fr       */
+/*   Updated: 2025/08/20 17:06:26 by gahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <iostream>
+#include "AForm.hpp"
+
+class RobotomyRequestForm : public AForm
+{
+	private:
+		std::string target;
+	public:
+		RobotomyRequestForm();
+		RobotomyRequestForm(std::string target);
+		RobotomyRequestForm(const RobotomyRequestForm &obj);
+		RobotomyRequestForm &operator=(const RobotomyRequestForm &obj);
+		~RobotomyRequestForm();
+
+		void finalExecution() const;
+		
+};
--- ex02/src/RobotomyRequestForm.cpp
+++ ex02/src/RobotomyRequestForm.cpp
@@ -6,8 +6,31 @@
 /*   By: gahmed <gahmed@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/05 12:43:16 by gahmed            #+#    #+#             */
-/*   Updated: 2025/08/05 14:08:40 by gahmed           ###   ########.fr       */
+/*   Updated: 2025/08/20 17:17:03 by gahmed           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/RobotomyRequestForm.hpp"
+
+RobotomyRequestForm::RobotomyRequestForm() : AForm("RobotomyRequestForm", 72, 45), target("Default")
+{}
+
+RobotomyRequestForm::RobotomyRequestForm(std::string target) : AForm("RobotomyRequestForm", 72, 45), target(target)
+{}
+
+RobotomyRequestForm::RobotomyRequestForm(const RobotomyRequestForm &obj) : AForm(obj), target(obj.target)
+{}
+
+RobotomyRequestForm &RobotomyRequestForm::operator=(const RobotomyRequestForm &obj)
+{
+	if (this != &obj)
+		target = obj.target;
+	return *this;
+}
+
+RobotomyRequestForm::~RobotomyRequestForm(){}
+
+void RobotomyRequestForm::finalExecution() const
+{
+	
+}
